dd3eb698db063673a907f4a1918a8df35f465ae4
ark <flukycoder@gmail.com>
2020-06-27 23:01:53 +0530

    merged

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const { mongoose } = require('./db/mongoose');
 const user = require('./routers/v1/user');
 const roast = require('./routers/v1/roast');
+const comment = require('./routers/v1/comment');
 
 const port = process.env.PORT;
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/v1', user);
 app.use('/api/v1', roast);
+app.use('/api/v1', comment);
 
 app.listen(port, () => {
   console.log(`Started On Port ${port}`);
