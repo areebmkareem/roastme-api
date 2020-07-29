6cca8f40df2b7ee639320cb805f0e4a173f6792f
ark <flukycoder@gmail.com>
2020-07-29 21:56:05 +0530

    try block made central and added errors middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,23 +1,20 @@
-const jwt = require("jsonwebtoken");
-const User = require("../modals/user");
-const auth = async (req, res, next) => {
-  try {
-    const token = req.header("token");
-    let decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded) {
-      let user = await User.findOne({
-        _id: decoded._id,
-        "tokens.token": token,
-      });
-      if (user) {
-        req.user = user;
-        req.token = token;
-        next();
-      } else res.send({ error: true, message: "User not found" });
-    } else res.send({ error: true, message: "token expired" });
-  } catch (error) {
-    res.send({ error: true, message: "you dont deserve!" });
-  }
-};
+const jwt = require('jsonwebtoken');
+const User = require('../modals/user');
+const asyncMiddleware = require('./async');
+const auth = asyncMiddleware(async (req, res, next) => {
+  const token = req.header('token');
+  let decoded = jwt.verify(token, process.env.JWT_SECRET);
+  if (decoded) {
+    let user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token,
+    });
+    if (user) {
+      req.user = user;
+      req.token = token;
+      next();
+    } else res.send({ error: true, message: 'User not found' });
+  } else res.send({ error: true, message: 'token expired' });
+});
 
 module.exports = auth;
---
+++ src/middleware/error.js
@@ -0,0 +1,3 @@
+const handleError = () => {};
+
+module.exports = handleError;
