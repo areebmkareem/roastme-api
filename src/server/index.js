b4a66ccd61ba57155aa55332974f63417c1b317b
ark <flukycoder@gmail.com>
2020-12-23 23:31:58 +0530

    rate limit added

--- src/server/index.js
+++ src/server/index.js
@@ -7,8 +7,21 @@ const cors = require('cors');
 const user = require('../routers/v1/user');
 const track = require('../routers/v1/track');
 const error = require('../controllers/error');
+const rateLimit = require('express-rate-limit');
 
 // winston.add(winston.transports.File, { filename: 'logfile.log' });
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
 
 const port = process.env.PORT;
 
