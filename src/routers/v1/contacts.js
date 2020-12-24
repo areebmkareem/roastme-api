3f8843cd7037bb901a7063371179892c2ab8856e
ark <flukycoder@gmail.com>
2020-12-24 23:05:26 +0530

    merged

--- src/routers/v1/contacts.js
+++ src/routers/v1/contacts.js
@@ -5,6 +5,9 @@ const User = require('../../models/user');
 const auth = require('../../controllers/auth');
 const isEmailVerified = require('../../controllers/emailVerified');
 
+const defaultLimit = 10;
+const defaultSkip = 0;
+
 /**
  * @api {GET} /contacts Get User Contacts
  * @apiGroup Contacts
@@ -17,10 +20,36 @@ router.get('/contacts', auth, isEmailVerified, async (req, res) => {
   const query = req.query;
   const projection = {isEmailVerified: 0, contacts: 0, userName: 0};
   const options = {
-    limit: parseInt(query.limit),
-    skip: parseInt(query.skip),
+    limit: parseInt(query.limit) || defaultLimit,
+    skip: parseInt(query.skip) || defaultSkip,
   };
-  const data = await User.find({_id: user.contacts}, projection).limit(options.limit).skip(options.skip);
+  let data = {
+    ...options,
+  };
+  data.contacts = await User.find({_id: user.contacts}, projection).limit(options.limit).skip(options.skip);
+  res.send({success: true, data});
+});
+
+/**
+ * @api {GET} /contacts/:userName Search User By User Name
+ * @apiGroup Contacts
+ * @apiName SearchUserByUserName
+ * @apiHeader {String} token  Mandatory users unique token.
+ */
+
+router.get('/contacts/:userName', auth, isEmailVerified, async (req, res) => {
+  const userName = req.params.userName;
+  const user = req.user;
+  const query = req.query;
+  const projection = {isEmailVerified: 0, contacts: 0, userName: 0};
+  const options = {
+    limit: parseInt(query.limit) || defaultLimit,
+    skip: parseInt(query.skip) || defaultSkip,
+  };
+  let data = {
+    ...options,
+  };
+  data.contacts = await User.find({userName}, projection).limit(options.limit).skip(options.skip);
   res.send({success: true, data});
 });
 
